8194987e6935ad79d9ceeba555908579b3fdebad
JohKenPo <joaovitormoraes.cc@gmail.com>
2022-10-29 19:47:20 -0300

    Foreach

---
+++ ArraysCollections/src/ForEach.java
@@ -0,0 +1,7 @@
+public class ForEach {
+    public static void main(String[] args) throws Exception {
+        int[] notas = {1,2,3,4,5,6};
+
+        for(int nota: notas)System.out.print(nota + " ");
+    }
+}
