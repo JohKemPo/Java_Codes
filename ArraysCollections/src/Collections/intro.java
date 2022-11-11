7c59043ef2848e54da85bd20023c06d4aa8660d1
JohKenPo <joaovitormoraes.cc@gmail.com>
2022-11-11 16:50:23 -0300

    Collections

---
+++ ArraysCollections/src/Collections/intro.java
@@ -0,0 +1,17 @@
+package Collections;
+
+public class intro {
+    public static void main(String[] args) {
+        /*
+         * Collections sao arrays q podem ter mais de um tipo de dado e nao precisam ser limitados ( n precisa especf o num de elementos na iniciação )
+         * 
+         * TIPOS DE COLLECTIONS
+         * 
+         * SET - Nao ordenado, nao indexado, nao aceita repeticao.
+         * LIST - Indexada, aceita repeticao
+         * MAP - Chave/Valor, Chave nao repetida, Valor aceita repeticao ( "Dicionario")
+         * QUEUE - Implementa a fila, FIFO ( first in/ First out )
+         * STACK - Implementa a pilha, LIFO ( Last in/ First out )
+         */
+    }
+}
