1f19279f44881c8ec5d789b7d8ee01c00d3d5916
JohKenPo <joaovitormoraes.cc@gmail.com>
2022-11-18 02:07:03 -0300

    interface

---
+++ POO/src/interfaces/intro.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public interface intro {
+    //class ou interface ok
+    /**
+     * A interface tera métodos porém sem corpo
+     * definida  qnd é preciso implementar algo na class qnd herda carac de uma interface
+     * "Cabeçalho"
+     * 
+     * 
+     * Extends ... implements interface
+     * 
+     * ela obriga a implementar algo.
+     */
+
+}
